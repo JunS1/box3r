112727220b858d0100fcedc0ff9a1246fcebb26f
largekuan <vkuan@uw.edu>
2020-10-17 21:34:52 -0700

    added static qr code page for placeholder

--- screens/Scanner/ScannerScreen.js
+++ screens/Scanner/ScannerScreen.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, Dimensions, StyleSheet } from 'react-native';
 import * as Location from 'expo-location';
+
+const win = Dimensions.get('window');
 
 export default class ScannerScreen extends React.Component {
 
@@ -23,8 +25,20 @@ export default class ScannerScreen extends React.Component {
     render() {
         return (
             <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <Text>Scanner</Text>
+                <Text style={styles.boldText}>Hold the camera</Text>
+                <Text style={styles.boldText}>to the QR sticker</Text>
+                <Image style={{margin: 90, width: win.width * 252/375, height: win.height * 252/812}}source={require('./QR_example.png')} />
+                <Text style={styles.boldText}>Scanning...</Text>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    boldText: {
+        fontFamily: (Platform.OS === 'ios') ? 'Helvetica' : 'Roboto',
+        fontWeight: "bold",
+        fontSize: 35,
+
+    },
+});
