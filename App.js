d03a1471420c5c7adcbeb000e6c75d33ca29fd83
JunS1 <jssong128@gmail.com>
2020-10-17 19:10:10 -0700

    added log in / register function

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import firebase from "./Firebase"
 
 import ProfileScreen from './screens/Profile/ProfileScreen'
 import BoxScreen from './screens/Box/BoxScreen'
 import ScannerScreen from './screens/Scanner/ScannerScreen'
+import LoginScreen from './screens/Authentication/LoginScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -15,17 +17,36 @@ export default class App extends React.Component {
     }
 
     componentDidMount() {
+        if (firebase.auth().currentUser) {
+            this.setState({ loggedIn: true });
+        } else {
+            firebase.auth().onAuthStateChanged(async (user) => {
+                if (user) {
+                    this.setState({
+                        loggedIn: true
+                    })
+                }
+            }) 
+        }
+    }
 
+    logIn = () => {
+        this.setState({ loggedIn: true });
     }
 
     render () {
         return (
             <NavigationContainer>
-                <Tab.Navigator> 
-                    <Tab.Screen name="Box" component={BoxScreen} />
-                    <Tab.Screen name="Scanner" component={ScannerScreen} />
-                    <Tab.Screen name="Profile" component={ProfileScreen} />
-                </Tab.Navigator>
+                {!this.state.loggedIn ? 
+                    <LoginScreen logIn={this.logIn} /> 
+                :
+                    <Tab.Navigator> 
+                        <Tab.Screen name="Box" component={BoxScreen} />
+                        <Tab.Screen name="Scanner" component={ScannerScreen} />
+                        <Tab.Screen name="Profile" component={ProfileScreen} />
+                    </Tab.Navigator>
+                }
+                
             </NavigationContainer>
         );
     }
